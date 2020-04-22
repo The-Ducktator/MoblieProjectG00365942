300257ca60dcbb99c326abd0e17cb83e4052954b
Eoin-Lernihan <48314832+Eoin-Lernihan@users.noreply.github.com>
2020-04-22 21:28:57 +0100

    Add files via upload

---
+++ src/app/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+  public getData(){
+
+    return this.httpClient.get('https://jsonplaceholder.typicode.com/todos');
+  }
+
+  public getHeader(){
+    return this.httpClient.get('https://jsonplaceholder.typicode.com/todos/1');
+
+  }
+}
